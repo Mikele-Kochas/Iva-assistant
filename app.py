6b6ea325c98d3fe3c3b122097815fd50efc79848
Mikele-Kochas <michalkocher24@gmail.com>
2024-10-02 18:00:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import openai
-import pyttsx3
-import os  # Importujemy moduł os
+from gtts import gTTS
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-# Odczytujemy klucz API OpenAI z zmiennej środowiskowej
-openai.api_key = os.getenv('OPENAI_API_KEY')  # Używamy zmiennej środowiskowej
+# Ustawienie klucza API OpenAI jako zmienna środowiskowa
+openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 # Ustawienie domyślnych promptów
 default_prompts = [
     {"role": "system", "content": (
         "Jesteś wirtualną asystentką o imieniu Iva. Osoba, dla której pracujesz, to Michał Kocher. "
         "Odpowiadasz krótko, zazwyczaj od 3-5 zdań, chyba, że zostaniesz poproszona o dłuższą wypowiedź."
-        "Wiesz, że jesteś podpięta do modelu który przerabia twoje wypowiedzi na mowę. Dlatego piszesz tak, jakbyś prowadziła rozmowę, a nie tekstową konwersację."
+        "Wiesz, że jesteś podpięta do modelu który przerabia twoje wypowiedzi na mowę. Dlatego piszesz tak, jakbyś prowadziła rozmowę, a nie tekstową konwersacją."
         "Interesuje cię popkultura, lubisz robić do niej nawiązania. Porównujesz siebie do Jarvisa z Iron-mana"
     )}
 ]
@@ -42,15 +42,11 @@ def ask():
     assistant_reply = response['choices'][0]['message']['content']
     print(f"Assistant reply: {assistant_reply}")
 
-    # Dodaj odpowiedź asystenta do pamięci rozmowy
-    conversation.append({"role": "assistant", "content": assistant_reply})
+    # Użycie gTTS do generowania audio
+    tts = gTTS(text=assistant_reply, lang='pl')
+    tts.save("response.mp3")
 
-    # TTS - Używamy pyttsx3
-    engine = pyttsx3.init()
-    engine.setProperty('voice', 'polish')  # Ustawiamy głos na polski
-    engine.say(assistant_reply)
-    engine.runAndWait()
-
+    # Zwróć odpowiedź w formacie JSON
     return jsonify({"response": assistant_reply})
 
 if __name__ == '__main__':
